392732466b56c55589ddb8a2e6cf5a99ebf873bb
Anuolu-2020 <Anuolu1645@gmail.com>
2024-10-12 23:42:22 +0100

    feat: sync meili configuration struct implemented

---
+++ internal/config/syncMeili.go
@@ -0,0 +1,32 @@
+package config
+
+type SyncMeiliConfig struct {
+	Database DatabaseConfig `yaml:"database"`
+	Sync     SyncConfig     `yaml:"sync"`
+}
+
+type SyncConfig struct {
+	Enabled    bool           `yaml:"enabled"`
+	Connection SyncConnection `yaml:"connection"`
+	Events     SyncEvents     `yaml:"events"`
+	Mappings   []SyncMapping  `yaml:"mappings"`
+}
+
+type DatabaseConfig struct {
+	Type string `yaml:"type"`
+}
+
+type SyncConnection struct {
+	MaxRetries   int    `yaml:"max_retries"`
+	RetryBackOff string `yaml:"retry_backoff"`
+}
+
+type SyncEvents struct {
+	EventTypes []string `yaml:"event_types"`
+}
+
+type SyncMapping struct {
+	DatabaseTable    string   `yaml:"database_table"`
+	MeilisearchIndex string   `yaml:"meilisearch_index"`
+	Fields           []string `yaml:"fields"`
+}
