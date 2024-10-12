1b6beb550f14f953bd7ee92807da456ac3634b9e
Anuolu-2020 <Anuolu1645@gmail.com>
2024-10-12 23:42:57 +0100

    feat: postgres connection implemented

---
+++ internal/db/postgres/postgres.go
@@ -0,0 +1,27 @@
+package postgres
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+type PostgresConnection struct {
+	Conn *sql.DB
+}
+
+func Connect(url string) (*PostgresConnection, error) {
+	conn, err := sql.Open("postgres", url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		return nil, err
+	}
+
+	return &PostgresConnection{conn}, nil
+}
+
+func (db *PostgresConnection) Close() {
+	db.Conn.Close()
+}
