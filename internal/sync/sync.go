9f798a3ab5b9144397933b8a1f15a58b898001fc
Anuolu-2020 <Anuolu1645@gmail.com>
2024-10-13 23:34:37 +0100

    feat: implemented logic for syncing meilisearch based on db operation action

--- internal/sync/sync.go
+++ internal/sync/sync.go
@@ -6,22 +6,26 @@ import (
 
 	"github.com/meilisearch/meilisearch-go"
 
+	"github.com/Anuolu-2020/sync-meili/internal/config"
 	"github.com/Anuolu-2020/sync-meili/pkg/env"
 )
 
-func ConnectToMeilisearch() *meilisearch.ServiceManager {
+func configureMeilisearchClient() meilisearch.ServiceManager {
 	client := meilisearch.New(
 		env.GetEnv("MEILISEARCH_CONN_STRING"),
 		meilisearch.WithAPIKey(env.GetEnv("MEILISEARCH_APIKEY")),
 	)
 
-	return &client
+	return client
 }
 
-func GetMeilisearchIndex(client meilisearch.ServiceManager) *meilisearch.IndexManager {
-	index := client.Index("")
+func GetMeilisearchIndex(
+	client meilisearch.ServiceManager,
+	indexUid string,
+) meilisearch.IndexManager {
+	index := client.Index(indexUid)
 
-	return &index
+	return index
 }
 
 func AddDocumentToMeilisearch(index meilisearch.IndexManager, documents []map[string]interface{}) {
@@ -59,4 +63,42 @@ func DeleteDocumentFromMeilisearch(
 	}
 
 	fmt.Printf("Delete document taskuid: %d", task.TaskUID)
+}
+
+func SyncToMeilisearch(config *config.SyncMeiliConfig, payload map[string]interface{}) {
+	client := configureMeilisearchClient()
+	var index meilisearch.IndexManager
+	document := make(map[string]interface{})
+	for _, mapping := range config.Sync.Mappings {
+		if mapping.DatabaseTable == payload["table"] {
+			for _, field := range mapping.Fields {
+				document[field] = payload["data"].(map[string]interface{})[field]
+			}
+			index = GetMeilisearchIndex(client, mapping.MeilisearchIndex)
+			switch payload["action"].(string) {
+			case "INSERT":
+				AddDocumentToMeilisearch(
+					index,
+					[]map[string]interface{}{document})
+
+			case "UPDATE":
+				UpdateDocumentInMeilisearch(
+					index,
+					mapping.MeilisearchIndexDocumentUid,
+					[]map[string]interface{}{document})
+
+			case "DELETE":
+				DeleteDocumentFromMeilisearch(
+					index,
+					mapping.MeilisearchIndexDocumentUid)
+			}
+
+		}
+	}
+	fmt.Printf(
+		"Table: %s, Action: %s, Data: %v\n",
+		payload["table"],
+		payload["action"],
+		payload["data"],
+	)
 }
