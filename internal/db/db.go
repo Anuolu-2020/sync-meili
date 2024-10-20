f3e540bbeb8d242e95d88464d05d3c4e24528380
Anuolu-2020 <Anuolu1645@gmail.com>
2024-10-20 23:34:32 +0100

    chore: use env url

--- internal/db/db.go
+++ internal/db/db.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Anuolu-2020/sync-meili/internal/config"
 	"github.com/Anuolu-2020/sync-meili/internal/db/mysql"
 	"github.com/Anuolu-2020/sync-meili/internal/db/postgres"
+	"github.com/Anuolu-2020/sync-meili/pkg/env"
 )
 
 type DBConnection struct {
@@ -18,14 +19,14 @@ var DBManager *DBConnection
 func InitDB(config *config.SyncMeiliConfig) {
 	switch config.Database.Type {
 	case "postgres":
-		conn, err := postgres.Connect("hello")
+		conn, err := postgres.Connect(env.GetEnv("DB_URL"))
 		if err != nil {
 			log.Fatalf("Failed to connect to Postgres: %v", err)
 		}
 
 		DBManager.Conn = conn.Conn
 	case "mysql":
-		conn, err := mysql.Connect("hello")
+		conn, err := mysql.Connect(env.GetEnv("DB_URL"))
 		if err != nil {
 			log.Fatalf("Failed to connect to Mysql: %v", err)
 		}
