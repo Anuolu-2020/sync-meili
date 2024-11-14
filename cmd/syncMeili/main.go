79124423f2482f68bfcce99f34c0ec42852887f3
Anuolu-2020 <Anuolu1645@gmail.com>
2024-11-14 23:49:03 +0100

    feat: implemented server and background job listening for postgres

--- cmd/syncMeili/main.go
+++ cmd/syncMeili/main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
 
 	"github.com/Anuolu-2020/sync-meili/internal/config"
 	"github.com/Anuolu-2020/sync-meili/internal/db"
 	"github.com/Anuolu-2020/sync-meili/internal/sync"
+	"github.com/Anuolu-2020/sync-meili/internal/webhook"
+	"github.com/Anuolu-2020/sync-meili/pkg/env"
 )
 
 func main() {
-	config, err := config.LoadConfig("config/config.yaml")
+	config, err := config.LoadConfig(".config/config.yaml")
 	if err != nil {
-		log.Fatalf("Failed to read sync-meili config: %v", err)
+		fmt.Printf("Failed to read sync-meili config: %v", err)
+		os.Exit(1)
 	}
 
 	// Initialize database
-	db.InitDB(config)
+	db.InitDB(*config)
 
 	// Initialize Syncing
 	sync.CreateTrigger(*config)
 
+	// Start syncing
+	go sync.ListenAndSync(config)
+	log.Print("Started listening for database updates")
+
 	defer db.DBManager.Conn.Close()
+
+	// Webhook endpoint
+	http.HandleFunc("/webhook", webhook.WebhookHandler)
+
+	// start server for webhook
+	log.Printf("Webhook server listening on http://localhost%s", env.GetEnv("PORT"))
+	http.ListenAndServe(env.GetEnv("PORT"), nil)
 }
