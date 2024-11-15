12712779ba9318e93592b09858051c89050eb002
Anuolu-2020 <Anuolu1645@gmail.com>
2024-11-15 23:53:02 +0100

    feat: started implementation of mysql syncing

--- internal/db/mysql/mysql.go
+++ internal/db/mysql/mysql.go
@@ -1,27 +1,47 @@
 package mysql
 
 import (
-	"database/sql"
 	"fmt"
 	"os"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-mysql-org/go-mysql/canal"
+	"github.com/siddontang/go-log/log"
+
+	"github.com/Anuolu-2020/sync-meili/internal/config"
+	"github.com/Anuolu-2020/sync-meili/pkg/env"
 )
 
 type MySqlConnection struct {
-	Conn *sql.DB
+	Canal *canal.Canal
 }
 
-func Connect(url string) (*MySqlConnection, error) {
-	conn, err := sql.Open("mysql", url)
+func Connect(config config.SyncMeiliConfig) (*MySqlConnection, error) {
+	cfg := canal.NewDefaultConfig()
+	cfg.Addr = env.GetEnv("MYSQL_ADDR")
+	cfg.User = env.GetEnv("MYSQL_USER")
+	cfg.Password = env.GetEnv("MYSQL_PASSWORD")
+	cfg.Dump.TableDB = env.GetEnv("MYSQL_TABLE_DB")
+	databaseTables := make([]string, 0)
+
+	// disable embedded canal logs
+	// cfg.Logger = log.NewDefault(&log.NullHandler{})
+
+	for _, mapping := range config.Sync.Mappings {
+		databaseTables = append(databaseTables, mapping.DatabaseTable)
+	}
+
+	cfg.Dump.Tables = databaseTables
+
+	c, err := canal.NewCanal(cfg)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		return nil, err
 	}
 
-	return &MySqlConnection{conn}, nil
+	return &MySqlConnection{Canal: c}, nil
 }
 
-func (db *MySqlConnection) Close() {
-	db.Conn.Close()
+func (canal MySqlConnection) Close() {
+	log.Print("closing mysql canal")
+	canal.Canal.Close()
 }
