6dedea8c135066e94fe06bb47533c01596a5c2ac
Anuolu-2020 <Anuolu1645@gmail.com>
2024-11-17 00:19:11 +0100

    feat: added new configurations

--- internal/config/syncMeili.go
+++ internal/config/syncMeili.go
@@ -1,25 +1,34 @@
 package config
 
+import "time"
+
 type SyncMeiliConfig struct {
 	Database DatabaseConfig `yaml:"database"`
 	Sync     SyncConfig     `yaml:"sync"`
 }
 
 type SyncConfig struct {
-	Enabled bool `yaml:"enabled"`
-	// Connection SyncConnection `yaml:"connection"`
-	Events   SyncEvents    `yaml:"events"`
-	Mappings []SyncMapping `yaml:"mappings"`
+	Enabled     bool              `yaml:"enabled"`
+	SyncRequest SyncRequestConfig `yaml:"sync_request"`
+	Batch       BatchConfig       `yaml:"batch"`
+	Events      SyncEvents        `yaml:"events"`
+	Mappings    []SyncMapping     `yaml:"mappings"`
 }
 
 type DatabaseConfig struct {
 	Type string `yaml:"type"`
 }
 
-// type SyncConnection struct {
-// 	MaxRetries   int    `yaml:"max_retries"`
-// 	RetryBackOff string `yaml:"retry_backoff"`
-// }
+type BatchConfig struct {
+	BatchSize     int           `yaml:"batch_size"`
+	FlushDuration time.Duration `yaml:"flush_duration"`
+}
+
+type SyncRequestConfig struct {
+	MaxRetries     int           `yaml:"max_retries"`
+	RetryDelay     time.Duration `yaml:"retry_delay"`
+	RequestTimeout time.Duration `yaml:"request_timeout"`
+}
 
 type SyncEvents struct {
 	EventTypes []string `yaml:"event_types"`
