3b7c7d748b1e937fd2699d8f8b1ec927f96031e3
Anuolu-2020 <Anuolu1645@gmail.com>
2024-11-18 23:31:50 +0100

    chore: commented out dumping databases for now

--- internal/db/mysql/mysql.go
+++ internal/db/mysql/mysql.go
@@ -21,16 +21,20 @@ func Connect(config config.SyncMeiliConfig) (*MySqlConnection, error) {
 	cfg.User = env.GetEnv("MYSQL_USER")
 	cfg.Password = env.GetEnv("MYSQL_PASSWORD")
 	cfg.Dump.TableDB = env.GetEnv("MYSQL_TABLE_DB")
-	databaseTables := make([]string, 0)
+	// databaseTables := make([]string, 0)
 
 	// disable embedded canal logs
 	// cfg.Logger = log.NewDefault(&log.NullHandler{})
+	// var db string
+	// dbs := make([]string, 0)
 
-	for _, mapping := range config.Sync.Mappings {
-		databaseTables = append(databaseTables, mapping.DatabaseTable)
-	}
-
-	cfg.Dump.Tables = databaseTables
+	// for _, mapping := range config.Sync.Mappings {
+	// 	// db = strings.Split(mapping.DatabaseTable, ".")[0]
+	// 	// dbs = append(dbs, db)
+	// 	databaseTables = append(databaseTables, mapping.DatabaseTable)
+	// }
+	//
+	// cfg.Dump.Tables = databaseTables
 
 	c, err := canal.NewCanal(cfg)
 	if err != nil {
